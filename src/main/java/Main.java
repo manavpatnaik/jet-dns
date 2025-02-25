bbc34034a98eafc38da3cbce7b28ed14d236c0af
Manav Patnaik <dev.manavpatnaik@gmail.com>
2025-02-25 10:38:14 +0530

    Set Packet ID and QR is response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,16 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 public class Main {
+
+    private static void setPacketId(byte[] response, int id) {
+        response[0] = (byte) ((id >> 8) & 0xFF);
+        response[1] = (byte) (id & 0xFF);
+    }
+
+    private static void setQR(byte[] response) {
+        response[2] |= (byte) (1 << 7);
+    }
+
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
@@ -17,6 +27,9 @@ public class Main {
          System.out.println("Received data");
 
          final byte[] bufResponse = new byte[512];
+         setPacketId(bufResponse, 1234);
+         setQR(bufResponse);
+
          final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length, packet.getSocketAddress());
          serverSocket.send(packetResponse);
        }
